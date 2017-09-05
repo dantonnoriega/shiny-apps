07d9ff937eb1995cbd283a20d896540302cc7dde
dantonnoriega <dantonnoriega@github.com>
2017-09-05 14:56:13 -0400

    working version

--- utm-parse/app.R
+++ utm-parse/app.R
@@ -1,68 +1,66 @@
-## Only run examples in interactive R sessions
-if (interactive()) {
-  
-  ui <- fluidPage(
-    titlePanel("Uploading Files"),
-    sidebarLayout(
-      sidebarPanel(
-        fileInput('file1', 'Choose file to upload',
-                  accept = c(
-                    'text/csv',
-                    'text/comma-separated-values',
-                    'app.Rtext/tab-separated-values',
-                    'text/plain',
-                    '.csv',
-                    '.tsv'
-                  )
-        ),
-        tags$hr(),
-        radioButtons("filetype", "Download File type:",
-                     choices = c("csv", "tsv")),
-        tags$hr(),
-        downloadButton('downloadData', 'Download')
+
+ui <- fluidPage(
+  titlePanel("Uploading Files"),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput('upload', 'Choose file to upload',
+                accept = c(
+                  'text/csv',
+                  'text/comma-separated-values',
+                  'app.Rtext/tab-separated-values',
+                  'text/plain',
+                  '.csv',
+                  '.tsv'
+                )
       ),
-      mainPanel(
-        tableOutput('table')
-      )
+      tags$hr(),
+      radioButtons("filetype", "Download File type:",
+                   choices = c("csv", "tsv")),
+      tags$hr(),
+      downloadButton('downloadData', 'Download')
+    ),
+    mainPanel(
+      tableOutput('table')
     )
   )
-  
-  server <- function(input, output) {
+)
 
-    datasetInput <- reactive({
-      
-      inFile <- input$file1
-      
-      if (is.null(inFile)) return(NULL)
-      
-      v <- get_utm(inFile$datapath)
-      return(v)
-      
-    })
+server <- function(input, output) {
+
+  datasetInput <- reactive({
     
+    inFile <- input$upload
     
-    output$table <- renderTable({
-      datasetInput()
-    })
+    if (is.null(inFile)) return(NULL)
     
-    output$downloadData <- downloadHandler(
-      
-      # This function returns a string which tells the client
-      # browser what name to use when saving the file.
-      filename = function() {
-        paste0('clean-', basename(input$file1$datapath))
-      },
-      
-      # This function should write data to a file given to it by
-      # the argument 'file'.
-      content = function(file) {
-        sep <- switch(input$filetype, "csv" = ",", "tsv" = "\t")
-        
-        # Write to a file specified by the 'file' argument
-        readr::write_delim(datasetInput(), path = file, delim = sep)
-      }
-    )
+    v <- get_utm(inFile$datapath)
+    return(v)
+    
+  })
   
-    shinyApp(ui, server)
-  }
+  
+  output$table <- renderTable({
+    datasetInput()
+  })
+  
+  output$downloadData <- downloadHandler(
+    
+    # This function returns a string which tells the client
+    # browser what name to use when saving the file.
+    filename = function() {
+      paste0('clean-', basename(input$upload$name))
+    },
+    
+    # This function should write data to a file given to it by
+    # the argument 'file'.
+    content = function(file) {
+      sep <- switch(input$filetype, "csv" = ",", "tsv" = "\t")
+      
+      # Write to a file specified by the 'file' argument
+      readr::write_delim(datasetInput(), path = file, delim = sep)
+    }
+  )
+
 }
+
+shinyApp(ui, server)
